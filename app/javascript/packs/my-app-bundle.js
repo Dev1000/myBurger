657e2ada7081b2b673e16d355c9eb07090c69de1
lalo <javaimagination@gmail.com>
2018-06-14 17:54:07 -0500

    Ready! rails 5.2.0 & react_on_rails 11.0.7

--- app/javascript/packs/my-app-bundle.js
+++ app/javascript/packs/my-app-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/myApp/HelloWorld';
+import HelloWorld from '../bundles/myApp/components/HelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
